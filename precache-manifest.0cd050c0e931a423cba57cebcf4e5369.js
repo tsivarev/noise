e320267432dccaba08186f9ad8b9b5ebbc88d8a5
Anton Tsivarev <a.tsivarev@corp.vk.com>
2019-05-17 19:27:40 +0300

    Updates

---
+++ precache-manifest.0cd050c0e931a423cba57cebcf4e5369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c06a94265967ec57bfa08e80cb81699",
+    "url": "/noise/index.html"
+  },
+  {
+    "revision": "88db43a9c65336da73b3",
+    "url": "/noise/static/css/2.b5769064.chunk.css"
+  },
+  {
+    "revision": "90c0bf35be1b5dfac93a",
+    "url": "/noise/static/css/main.553ad7fd.chunk.css"
+  },
+  {
+    "revision": "88db43a9c65336da73b3",
+    "url": "/noise/static/js/2.02967c49.chunk.js"
+  },
+  {
+    "revision": "90c0bf35be1b5dfac93a",
+    "url": "/noise/static/js/main.784ed040.chunk.js"
+  },
+  {
+    "revision": "1ab92e55ea01b1009d0e",
+    "url": "/noise/static/js/runtime~main.6de52e69.js"
+  },
+  {
+    "revision": "ebfdc4a07987ec3249bfceac911d9850",
+    "url": "/noise/static/media/logo.ebfdc4a0.svg"
+  }
+]);
